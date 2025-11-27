6985455cb4dae8bbbb45164237c1753bedfeb7aa
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-11-27 21:26:58 +0600

    FsModule Crate

---
+++ fsModule/index.js
@@ -0,0 +1,21 @@
+var fs =require('fs')
+var http =require('http')
+
+
+var server = http.createServer(function(req,res){
+
+
+    if(req.url='/'){
+        fs.readFile('Home.html',function(error,data){
+                     
+            res.writeHead(200,{'Content-type':'text/html'})
+            res.write(data)
+            res.end();
+        });
+    }
+
+});
+
+
+server.listen(5050);
+console.log("server Run seccessfull");

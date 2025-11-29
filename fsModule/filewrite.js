0fdb83cf3910a8c8b7df1ead425694e884737a89
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-11-29 19:22:53 +0600

    File Write in Fs Module

---
+++ fsModule/filewrite.js
@@ -0,0 +1,33 @@
+var fs = require('fs')
+var http = require('http')
+
+
+var server =http.createServer(function(req,res){
+
+   if(req.url='/'){
+
+         fs.writeFile('demo.txt','Hello World',function(error){
+            if(error){ 
+            res.writeHead(200,{'Content-type':'text/html'});
+            res.write("File Write Fail");
+            res.end();
+            } 
+            else{
+                res.writeHead(200,{'Content-type':'text/html'});
+            res.write("File Write Success");
+            res.end();
+
+            }
+
+
+             
+         });
+
+   }
+
+
+});
+
+server.listen(5050);
+console.log("Server Run Seccess");
+

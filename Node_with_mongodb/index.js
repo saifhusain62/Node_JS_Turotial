09af3fd066189faf761c13aead1b035cbf06c056
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-12-20 00:08:15 +0600

    Node Connect with Mongo Db & collection success

--- Node_with_mongodb/index.js
+++ Node_with_mongodb/index.js
@@ -5,12 +5,33 @@ var URL = "mongodb+srv://saifdemo:saif0000@cluster0.3urofnd.mongodb.net/?retryWr
 
 var config = { useUnifiedTopology: true } 
 
-MongoClient.connect(URL,config,function(error){
+MongoClient.connect(URL,config,function(error,MyMongoClient){
 
     if(error){
         console.log("Connection Failed");
     }
     else{
         console.log("Connection Success")
+        InsertData(MyMongoClient);
     }
 })
+
+function InsertData(MyMongoClient){
+   var MyDataBase = MyMongoClient.db("school")
+  var Mycollection = MyDataBase.collection("students")
+
+   var MyData = {
+    name:"Saif",
+    Roll:"01",
+    Class:10,
+    City:"Dhaka"
+   }
+   Mycollection.insertOne(MyData,function(error){
+    if(error){
+        console.log("Insert Failed")
+    }
+    else{
+        console.log("Insert Success");
+    }
+   })
+}

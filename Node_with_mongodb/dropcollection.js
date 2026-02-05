1926837374704ec9d36764e7fe45249cbe037bf8
saifhusain62 <148823252+saifhusain62@users.noreply.github.com>
2026-02-05 21:36:14 +0600

    Drop collection with Mongo DB

---
+++ Node_with_mongodb/dropcollection.js
@@ -0,0 +1,24 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var URL = "mongodb+srv://saifhusain4051_db_user:saif0000@cluster0.rfiwbtz.mongodb.net/?retryWrites=true&w=majority";
+
+var config = { useUnifiedTopology: true };
+
+MongoClient.connect(URL,config,function(error,MymongoClient){
+    if(error){
+        console.log("Connection failed");
+    }
+    else{
+        console.log("Connection Success");
+        DropCollection(MymongoClient);
+    }
+})
+
+function DropCollection(MymongoClient){
+    var MyDataBase = MymongoClient.db("versity");
+
+    MyDataBase.dropCollection("class",function(error,result){
+        console.log(result);
+    })
+
+}
